bb41ea01f666d216432fb483bc606b5aa9058670
User <bekhzod.asadullayev@gmail.com>
2022-02-14 21:51:46 +0500

    First Commit

---
+++ src/main/java/uz/pdp/eticketdemo/ETicketDemoApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.eticketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETicketDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ETicketDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/eticketdemo/ETicketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.eticketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETicketDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
