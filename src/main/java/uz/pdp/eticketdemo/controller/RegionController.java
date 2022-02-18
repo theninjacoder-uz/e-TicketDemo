229d2434b7430c7c352fa74db3fccfc1797e3b27
madina <bonushirinkulova@gmail.com>
2022-02-18 18:24:29 +0500

    finished: region and country

---
+++ src/main/java/uz/pdp/eticketdemo/controller/RegionController.java
@@ -0,0 +1,34 @@
+package uz.pdp.eticketdemo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import uz.pdp.eticketdemo.model.dto.CountryDto;
+import uz.pdp.eticketdemo.model.dto.RegionDto;
+import uz.pdp.eticketdemo.response.ApiResponse;
+import uz.pdp.eticketdemo.service.RegionService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/region")
+public class RegionController {
+    private final RegionService service;
+    @PostMapping("/add")
+    public ApiResponse add(@RequestBody RegionDto regionDto){
+        return service.add(regionDto);
+    }
+
+    @GetMapping("/list")
+    public ApiResponse getList(){
+        return service.getList();
+    }
+
+    @GetMapping("/get")
+    public ApiResponse getById(@RequestParam(name = "id") Long id){
+        return service.getById(id);
+    }
+
+    @PostMapping("/edit")
+    public ApiResponse edit(@RequestParam(name = "id") Long id, @RequestBody RegionDto regionDto){
+        return service.edit(id, regionDto);
+    }
+}
