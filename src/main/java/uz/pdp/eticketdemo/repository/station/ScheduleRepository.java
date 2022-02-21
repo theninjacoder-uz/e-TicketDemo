b0adb67b781244893d5e4c415ae95011e99904f0
user <m.muhammadsodiq777@gmail.com>
2022-02-21 09:04:31 +0500

    schedule

--- src/main/java/uz/pdp/eticketdemo/repository/station/ScheduleRepository.java
+++ src/main/java/uz/pdp/eticketdemo/repository/station/ScheduleRepository.java
@@ -10,4 +10,6 @@ public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> 
 
     @Query(value = "select * from station st join address ad on where st.id = ?1 or st.id =?2 ", nativeQuery = true)
     List<Object> getAllSchedule(Long fId, Long tId);
+
+//    List<Object> find
 }
