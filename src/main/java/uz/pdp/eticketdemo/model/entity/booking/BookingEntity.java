39c0372e7f4c3b2382240206939a0a7c976edab1
User <bekhzod.asadullayev@gmail.com>
2022-02-23 22:58:20 +0500

    BookingEntity added

---
+++ src/main/java/uz/pdp/eticketdemo/model/entity/booking/BookingEntity.java
@@ -0,0 +1,23 @@
+package uz.pdp.eticketdemo.model.entity.booking;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Table
+@Entity(name = "booking")
+public class BookingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private long trainId;
+    private long wagonId;
+    private long seatId;
+    private LocalDateTime localDateTime;
+
+    private long seatOccupationInterval;
+}
